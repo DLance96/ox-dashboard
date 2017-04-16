9b11e6748eb7860f43031a59b72ee1fa02b6a1ce
Matthew Bentley <matthew@bentley.link>
2017-04-16 01:58:22 -0400

    Wait for network to start?

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+while ! ping -c1 8.8.8.8 &>/dev/null; do echo "waiting for net"; sleep 1; done
+
 cp /db/db.sqlite3 /db/db.sqlite3.bak.$(date '+%y-%m-%d_%H-%M-%S')
 python manage.py migrate
 python manage.py runserver 0.0.0.0:80
-
