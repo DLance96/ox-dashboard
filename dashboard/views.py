e115a24f245be248c6323d0921177fbc1e8027bd
ClefAdagio <74155476+ClefAdagio@users.noreply.github.com>
2020-11-29 15:57:07 -0500

    Added brother and candidate count to the Brother Info page at the bottom of Academic Info (#70)

--- dashboard/views.py
+++ dashboard/views.py
@@ -107,9 +107,13 @@ def brother_info_list(request):
 
     """ Renders brother info page """
     brothers = Brother.objects.order_by("brother_status", "last_name", "first_name")
+    brother_count = Brother.objects.filter(brother_status="1").count()
+    candidate_count = Brother.objects.filter(brother_status="0").count()
 
     context = {
         'brothers': brothers,
+        'brother_count': brother_count,
+        'candidate_count': candidate_count,
     }
     return render(request, 'brother-info-list.html', context)
 
